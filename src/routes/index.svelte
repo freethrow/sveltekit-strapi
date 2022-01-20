79eb7ae9590c483315762e2c77738664752dd279
freethrow <aleksendric@gmail.com>
2022-01-20 19:10:14 +0100

    fixed index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,3 @@
-<script context="module">
-
-    export async function load({fetch}){
-
-        const res = await fetch('https://ft-strapi-reviews.herokuapp.com/api/articles?populate=*')
-
-        const jsonRes = await res.json()
-        console.log(jsonRes.data)
-        return {
-            props:{
-                articles: jsonRes.data
-            }
-            
-        }
-
-       
-    }
-
-</script>
-
-<script>
-    export let articles
-</script>
-
-
-
 <div>
     <h1>SvelteKit + Strapi News ooh</h1>
 </div>
