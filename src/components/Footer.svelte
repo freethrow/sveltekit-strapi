9b8b071ad7c61e152b040a9a2e75a5bb17c923f9
freethrow <aleksendric@gmail.com>
2022-01-21 18:43:40 +0100

    small pictures

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,3 @@
-<div class="bg-pink-900 text-white font-thin p-3">
+<div class="text-pink-900 font-thin p-3">
     Lorem pipsum 2022 sveltekit
 </div>
--- src/routes/articles.svelte
+++ src/routes/articles.svelte
@@ -5,6 +5,8 @@
         const res = await fetch('https://ft-strapi-reviews.herokuapp.com/api/articles?populate=*')
 
         const jsonRes = await res.json()
+
+        console.log(jsonRes.data)
     
         return {
             props:{
@@ -30,7 +32,8 @@
     {#each articles as article}
         <div class="shadow-lg">
             
-            <img class=" opacity-50 hover:opacity-100 transition duration-1000" src="{article.attributes.cover.data.attributes.url}" alt="{article.attributes.cover.data.attributes.name}">
+            <img class=" opacity-50 hover:opacity-100 transition duration-1000 w-full" 
+                src="{article.attributes.cover.data.attributes.formats.small.url}" alt="{article.attributes.cover.data.attributes.name}">
             <div class="p-5">
             <h2 class="text-pink-700 font-semibold text-center">{article.attributes.title}</h2>
             <div>{article.attributes.body}</div>
